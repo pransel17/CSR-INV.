b3b90d08880e59a1fd6794072d9c5a4883a57693
Kielazar (Jez) <131634394+Kielazar@users.noreply.github.com>
2025-01-30 15:34:28 +0800

    Update pythontry.py

--- pythontry.py
+++ pythontry.py
@@ -9,6 +9,8 @@ radius = float(input("Enter the radius of the circle: "))
 area = circle_area(radius)
 print(f"The area of the circle is: {area:.2f}")
 
+print(f"Ang area ng circle natoh ay: {area:.2f}")
+
 
 
 
