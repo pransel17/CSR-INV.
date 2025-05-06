b107012dd1fe28ec0e0b3d3518b4e5c1a0e1e056
Francelle Sarayan <137190466+pransel17@users.noreply.github.com>
2025-05-06 15:32:43 +0800

    Add files via upload

---
+++ database/student_info.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2025 at 08:12 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_info`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student information`
+--
+
+CREATE TABLE `student information` (
+  `STUDENT NUMBER` int(11) UNSIGNED NOT NULL,
+  `STUDENT NAME` varchar(100) NOT NULL,
+  `SECTION` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','student') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `password`, `role`) VALUES
+('admin@labtrackCDM.edu', '$2b$12$zAeYgT7bKPuewkJ.x2JKHeXrJWhMEihd1CMa/pwiOLoDZKZkGM7cO', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student information`
+--
+ALTER TABLE `student information`
+  ADD PRIMARY KEY (`STUDENT NUMBER`),
+  ADD UNIQUE KEY `Name` (`STUDENT NAME`),
+  ADD KEY `Section` (`SECTION`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
